b3503fa537abd02bc1a955c0c721872597ec097b
root <root@localhost>
2023-12-19 18:04:45 +0800

    auto publish 1702980285237

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import Android
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'class of titans.settings')
+    android.environ.setdefault('DJANGO_SETTINGS_MODULE', 'class of titans.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
